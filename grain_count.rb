125a8347bf08e07c017e10eee1f62c28f9df1f58
astravisual <vlad@hotmail.co.nz>
2017-06-13 01:17:25 -0700

    exercism.io grains problem need to come back to this to refactor tbh

---
+++ grain_count.rb
@@ -0,0 +1,21 @@
+def grain_count
+board = []
+squares = 1 
+
+while board.length != 64 
+ board.push(squares)
+  squares += 1
+
+end 
+
+ grain = 1 
+
+board.each do |square|
+  
+  if grain == grain 
+    grain = grain * 2 
+  end
+  puts "This is Square#{square}:  #{grain} grains" 
+end 
+end 
+  grain_count 
